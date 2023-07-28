5d18dbfaf4e62d41a476a7b866191231f3f52360
PatrickSAE <p.fischer@sae.edu>
2023-07-29 01:48:46 +0200

    WeaponSystem Prefab created. Should be tested

--- Assets/Scripts/WeaponSystem/Bullet.cs
+++ Assets/Scripts/WeaponSystem/Bullet.cs
@@ -1,21 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Security.Cryptography;
 using UnityEngine;
+using WeaponSystem;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
-    
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float damage;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ParticleSystem interactionParticle;
+
+    private Rigidbody _rigidbody;
+
+    public event Action OnHittetObject;
+
+    void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        _rigidbody.AddForce(transform.forward * bulletSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        var myTransform = transform;
+
+        if (interactionParticle != null)
+        {
+            Instantiate(interactionParticle, myTransform.position, myTransform.rotation);
+        }
         
+        OnHittetObject?.Invoke();
+
+        var hittetObject = other.GetComponent<IHittable>();
+
+        if (hittetObject != null)
+        {
+            hittetObject.OnHit(damage);
+        }
     }
 }
---
+++ Assets/Scripts/WeaponSystem/DestroyableObject.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+namespace WeaponSystem
+{
+    public class DestroyableObject : MonoBehaviour, IHittable
+    {
+        [SerializeField] private float maxHealth;
+        
+        [Header("Optional")]
+        [SerializeField] private GameObject destroyedVersion;
+        [SerializeField] private ParticleSystem _deathParticle;
+
+        private float _currentHealth;
+
+        public bool IsDeath => _currentHealth <= 0;
+
+        public event Action OnDeath; 
+        public event Action OnGettingHit;
+        
+        //needs expanded with sounds that will be fired
+
+        private void Awake()
+        {
+            _currentHealth = maxHealth;
+        }
+
+        public void OnHit(float damage)
+        {
+            if (IsDeath)
+            {
+                return;
+            }
+            
+            _currentHealth -= damage;
+
+            _currentHealth = Mathf.Max(_currentHealth, 0);
+
+            if (IsDeath)
+            {
+                var myTransform = transform;
+                
+                if (destroyedVersion)
+                {
+                    Instantiate(destroyedVersion, myTransform.position, myTransform.rotation);
+                }
+
+                if (_deathParticle)
+                {
+                    Instantiate(_deathParticle, myTransform.position, myTransform.rotation);
+                }
+                
+                Destroy(gameObject);
+                OnDeath?.Invoke();
+            }
+            else
+            {
+                OnGettingHit?.Invoke();
+            }
+        }
+    }
+}
