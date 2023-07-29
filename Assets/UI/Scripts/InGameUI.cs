e0b2de34dd438961b7cf32840f76d8e4048ee61c
Ines <I.Volkmann07@gmail.com>
2023-07-29 23:38:51 +0200

    *Added Failed Panel to Prefabs

--- Assets/UI/Scripts/InGameUI.cs
+++ Assets/UI/Scripts/InGameUI.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InGameUI : MonoBehaviour
 {
-
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
