1ebb219f19c577836f2154c1f2bf75192b235ff6
Ines <I.Volkmann07@gmail.com>
2023-07-29 07:23:02 +0200

    * Timer works

---
+++ Assets/UI/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float m_timer = 0.0f;
+    private TextMeshProUGUI m_timerText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_timerText = GetComponent<TextMeshProUGUI>();
+        m_timer = 0.0f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+        //Debug.Log(m_timer.ToString());
+
+        //m_timerText.text = m_timer.ToString();
+        DisplayTime();
+    }
+
+    private void DisplayTime()
+    {
+        var minutes = Mathf.FloorToInt(m_timer / 60);
+        var seconds = Mathf.FloorToInt(m_timer % 60);
+        var fraction = m_timer % 1 * 1000;
+
+        m_timerText.text = string.Format("{0:00}:{1:00},{2:00}", minutes, seconds, fraction);
+    }
+}
